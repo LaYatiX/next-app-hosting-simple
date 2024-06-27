34652b9ba95bbbf7588fb450c020899e6874f5b6
gpiwosz <gpiwosz@gmail.com>
2024-06-27 15:57:54 +0200

    test data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const message = await getData()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          <code className="font-mono font-bold">{message}</code> FROM SERVER!
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -111,3 +112,26 @@ export default function Home() {
     </main>
   );
 }
+
+
+async function getData() {
+  const res = await fetch('https://us-central1-nest-node-mailer.cloudfunctions.net/api')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.text()
+}
+ 
+
+// export const getServerSideProps = (async () => {
+//   // Fetch data from external API
+//   const res = await fetch('https://us-central1-nest-node-mailer.cloudfunctions.net/api')
+//   const message: string = await res.text()
+//   // Pass data to the page via props
+//   return { props: { message: message } }
+// }) satisfies GetServerSideProps<{ message: string }>
